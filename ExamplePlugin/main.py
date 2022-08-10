5c8efda6e5b818837261bdc434a8e2b924a456ec
Christopher <cpsuperstoreinc@gmail.com>
2022-08-09 21:20:20 -0400

    Create main.py

---
+++ ExamplePlugin/main.py
@@ -0,0 +1,18 @@
+def on_start():
+    print("Start")
+
+
+def on_close():
+    print("Close")
+
+
+def on_command(command, name, guest):
+    print("Command", command, name, guest)
+
+
+def on_console_clear():
+    print("Console Cleared")
+
+
+def on_rewards_pull(rewards):
+    print("Rewards Updated")
